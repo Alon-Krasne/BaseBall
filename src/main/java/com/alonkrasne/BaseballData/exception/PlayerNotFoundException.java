68f077be5a39880b32bb3816e4635f796dda925f
Alon-Krasne <alonkrasne@gmail.com>
2023-05-16 10:18:41 +0300

    Conform to standards

--- src/main/java/com/alonkrasne/BaseballData/exception/PlayerNotFoundException.java
+++ src/main/java/com/alonkrasne/BaseballData/exception/PlayerNotFoundException.java
@@ -1,9 +1,9 @@
 package com.alonkrasne.BaseballData.exception;
 
 public class PlayerNotFoundException extends RuntimeException {
-    private static final String idNotFound = "Player with id %s not found";
+    private static final String ID_NOT_FOUND = "Player with id %s not found";
 
     public PlayerNotFoundException(String id) {
-        super(String.format(idNotFound, id));
+        super(String.format(ID_NOT_FOUND, id));
     }
 }
